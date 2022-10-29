49519f2e6923ee4eb7fb97b5bf67960e6f9b6485
Ilavarasan.T <ilavarasan1623@gmail.com>
2022-10-29 11:34:27 +0530

    login page validation chnages

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,24 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { faLock } from '@fortawesome/free-solid-svg-icons';
-import { SharedWebAPIService } from 'src/app/shared-web-api.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
- 
+  isShowLoader: boolean = false;
+  patternname="^[a-zA-Z]+$";
+  numpattern="/^[0-9]+$/";
+  form = new FormGroup({                                               
+    
+    email: new FormControl('', [Validators.required,Validators.email]),
+    password:new FormControl ('',[Validators.required,Validators.minLength(8),Validators.maxLength(20), Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-zd$@$!%*?&].{8,20}')]),
+   
+  });
 
-  constructor(private httpservice:SharedWebAPIService) { }
- 
+  constructor() { }
+
   ngOnInit(): void {
    
   }
-  onSubmit(){
-    debugger
-    this.httpservice.getRegisterList().subscribe(res=>{
-      alert(res.toString())
-    });
+  onSubmit() {
+   
+    this.form.markAllAsTouched();
+    if (this.form.valid) {
+      this.isShowLoader = true;
+      
+      
+         alert("Login Success !!!");
+
+         
+        }
+        else {
+          //alert("Invalid Credentials!!!");
+        }
+    
+    }
   }
 
-}
+
