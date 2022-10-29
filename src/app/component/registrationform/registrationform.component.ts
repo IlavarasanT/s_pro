073071534875e1a830d0cc2a79df27573d63731b
Ilavarasan.T <ilavarasan1623@gmail.com>
2022-10-29 13:35:30 +0530

    registrayion page changes

--- src/app/component/registrationform/registrationform.component.ts
+++ src/app/component/registrationform/registrationform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registrationform',
@@ -6,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrationform.component.css']
 })
 export class RegistrationformComponent implements OnInit {
+  public fullName:string='';
   constructor() { }
-
+  user_name: string = '';
+  
+  isShowLoader: boolean = false;
+  patternname="^[a-zA-Z]+$";
+  numpattern="/^[0-9]+$/";
+  form = new FormGroup({                                               
+     fname: new FormControl('', [Validators.required, Validators.minLength(3),Validators.pattern(this.patternname)]),
+     lname: new FormControl('', [Validators.required, Validators.minLength(3),Validators.pattern(this.patternname)]),
+    phnm: new FormControl('', [Validators.required, Validators.pattern('[0-9]*'),Validators.minLength(10),Validators.maxLength(10)]),
+    email: new FormControl('', [Validators.required,Validators.email]),
+    password:new FormControl ('',[Validators.required,Validators.minLength(8),Validators.maxLength(20), Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-zd$@$!%*?&].{8,20}')]),
+    add: new FormControl('', [Validators.required, Validators.minLength(3)])
+  });
+  
+  clickme() {
+    console.log('it does nothing',this.user_name);
+  }
   ngOnInit(): void {
   }
- 
+  addRegister(){
+    var values=this.user_name;
+    console.log(values);
+  }
+  onSubmit() {
+   
+    this.form.markAllAsTouched();
+    if (this.form.valid) {
+      this.isShowLoader = true;
+      
+      
+         alert("Login Success !!!");
+
+         
+        }
+        else {
+          //alert("Invalid Credentials!!!");
+        }
+    
+    }
+
 }
